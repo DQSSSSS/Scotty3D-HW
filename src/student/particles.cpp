6476b883cf604e3e513b84fdf6f276fca8836673
Max Slater <mjslater@andrew.cmu.edu>
2021-09-01 16:16:26 -0400

    Update particles.cpp

--- src/student/particles.cpp
+++ src/student/particles.cpp
@@ -10,10 +10,10 @@ bool Scene_Particles::Particle::update(const PT::Object& scene, float dt, float 
 
     // (1) Build a ray representing the particle's path if it travelled at constant velocity.
 
-    // (2) Intersect the ray with the scene and account for collisions. Be careful when place
-    // collision points based on the particle radius. Move the particle to its next position.
+    // (2) Intersect the ray with the scene and account for collisions. Be careful when placing
+    // collision points using the particle radius. Move the particle to its next position.
 
-    // (3) Account for acceleration.
+    // (3) Account for acceleration due to gravity.
 
     // (4) Repeat until the entire time step has been consumed.
 
